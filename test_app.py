5831a12b5fb38a6f51d25b06d8f083c312831a49
Deepak Kumar <Deepak.kr545@live.com>
2025-08-05 13:47:44 +0530

    Code Updted to test case

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_home():
     tester = app.test_client()
     response = tester.get('/')
     assert response.status_code == 200
-    assert response.data == b'Test Passed: Hello from Jenkins CI/CD Pipeline!'
+    assert response.data == b'Hello from Jenkins CI/CD Pipeline!'
